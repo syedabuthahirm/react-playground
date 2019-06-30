d837ecd8f8dc0d88a47ac7e91f9eb469f49b07bb
syedabuthahirm <abuthahir741@gmail.com>
2019-06-30 23:42:45 +0530

    Bug Fix in Auto Select

--- src/AutoSelect.js
+++ src/AutoSelect.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
-import Select from 'react-select';
 import CreatableSelect from 'react-select/creatable'
-import { emphasize, makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import NoSsr from '@material-ui/core/NoSsr';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import Chip from '@material-ui/core/Chip';
 import MenuItem from '@material-ui/core/MenuItem';
-import CancelIcon from '@material-ui/icons/Cancel';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(theme => ({
@@ -221,14 +216,6 @@ export default function IntegrationReactSelect(props) {
         <CreatableSelect
           classes={classes}
           styles={selectStyles}
-          TextFieldProps={{
-            label: 'Country',
-            InputLabelProps: {
-              htmlFor: 'react-select-single',
-              shrink: true,
-            },
-            placeholder: 'Search a country (start with a)',
-          }}
           components={components}
           {...props}
         />
